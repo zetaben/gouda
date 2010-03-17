2a93e3332a6f5a19ab6e4c41c2ac66c74eeffc50
Benoit Larroque <zeta.ben@gmail.com>
2010-03-18 00:14:10 +0100

    mysql connected !

--- MysqlConnection.go
+++ MysqlConnection.go
@@ -4,27 +4,48 @@ import (
 	"mysql"
 	"strings"
 	"fmt"
+	"os"
 )
 
 type mysqlConnector struct {
-	conn * mysql.MySQLInstance
+	conn *mysql.MySQLInstance
 }
 
 
-func (*mysqlConnector ) Open(conStr string) bool {
+func (e *mysqlConnector) Open(conStr string) bool {
 	fmt.Println("Plop")
-	tab:=strings.Split(conStr,"/",0)
-	fmt.Println(tab[2])
+	tab := strings.Split(conStr, "/", 0)
+	db := tab[len(tab)-1]
+	tab2 := strings.Split(tab[2], "@", 2)
+	tab = strings.Split(tab2[0], ":", 0)
+	fmt.Println(tab)
+	fmt.Println(tab2)
+	user := tab[0]
+	pass := tab[1]
+
+	dbh, err := mysql.Connect("tcp", "", tab2[1], user, pass, "")
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		os.Exit(1)
+	}
+	e.conn = dbh
+	e.conn.Use(db)
 	return false
 }
 
-func (*mysqlConnector ) Query(conStr string) string {
-	fmt.Println("Plip")
+func (e *mysqlConnector) Query(r *Relation) string {
+	res, err := e.conn.Query(r.Sql())
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(res)
+
 	return "plip"
 }
 
 func OpenMysql(conStr string) *mysqlConnector {
-	db:= new(mysqlConnector)
+	db := new(mysqlConnector)
 	db.Open(conStr)
 	return db
 }
